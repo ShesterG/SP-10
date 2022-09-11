6c3a9098817496abecae436e1c2f0d8404c8ff16
ShesterG <64415241+ShesterG@users.noreply.github.com>
2022-09-11 10:01:51 +0000

    frames source

--- video_converter.py
+++ video_converter.py
@@ -16,6 +16,55 @@ extractedInformation = pytesseract.image_to_string(Image.open('2.jpg'))
 
 #TO BE ADDED Shester ; ocr end
 
+
+
+#TO BE ADDED: SHESTER getting frames start
+
+# Importing all necessary libraries
+import cv2
+import os
+
+# Read the video from specified path
+vid = cv2.VideoCapture("E:\my Computer Vision\OpenCV\Resources/vid1.mp4")
+
+try:
+
+    # creating a folder named data
+    if not os.path.exists('data'):
+        os.makedirs('data')
+
+# if not created then raise error
+except OSError:
+    print('Error: Creating directory of data')
+
+# frame
+currentframe = 0
+
+while (True):
+
+    # reading from frame
+    success, frame = vid.read()
+
+    if success:
+        # continue creating images until video remains
+        name = './data/frame' + str(currentframe) + '.jpg'
+        print('Creating...' + name)
+
+        # writing the extracted images
+        cv2.imwrite(name, frame)
+
+        # increasing counter so that it will
+        # show how many frames are created
+        currentframe += 1
+    else:
+        break
+
+# Release all space and windows once done
+vid.release()
+cv2.destroyAllWindows()
+
+#TO BE ADDED: SHESTER getting frames end
+
 import logging
 import argparse
 from pathlib import Path

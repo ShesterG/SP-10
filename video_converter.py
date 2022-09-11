d77df98a469993e80df04c5dbf27f5489714fd96
ShesterG <64415241+ShesterG@users.noreply.github.com>
2022-09-11 11:40:44 +0000

    added frames and folder to insert them

--- video_converter.py
+++ video_converter.py
@@ -1,69 +1,16 @@
+#frames from videos; https://github.com/Ai-Genome-Saksham/OpenCV/blob/main/OpenCV/%239%20Extracting%20Images%20from%20Video.py
+#text from images;   https://github.com/bhadreshpsavani/ExploringOCR/blob/master/OCRusingTesseract.ipynb
 
-#TO BE ADDED Shester ; ocr start
-!sudo apt install tesseract-ocr
-!pip install pytesseract
+!sudo apt install tesseract-ocr # TODO : INSERT INSIDE REQUIREMENTS ?
+!pip install pytesseract        # TODO : INSERT INSIDE REQUIREMENTS ?
 
 import pytesseract
-import shutil
-import os
 import random
 try:
     from PIL import Image
 except ImportError:
     import Image
 
-extractedInformation = pytesseract.image_to_string(Image.open('2.jpg'))
-
-#TO BE ADDED Shester ; ocr end
-
-
-
-#TO BE ADDED: SHESTER getting frames start
-
-# Importing all necessary libraries
-import cv2
-import os
-
-# Read the video from specified path
-vid = cv2.VideoCapture("E:\my Computer Vision\OpenCV\Resources/vid1.mp4")
-
-try:
-
-    # creating a folder named data
-    if not os.path.exists('data'):
-        os.makedirs('data')
-
-# if not created then raise error
-except OSError:
-    print('Error: Creating directory of data')
-
-# frame
-currentframe = 0
-
-while (True):
-
-    # reading from frame
-    success, frame = vid.read()
-
-    if success:
-        # continue creating images until video remains
-        name = './data/frame' + str(currentframe) + '.jpg'
-        print('Creating...' + name)
-
-        # writing the extracted images
-        cv2.imwrite(name, frame)
-
-        # increasing counter so that it will
-        # show how many frames are created
-        currentframe += 1
-    else:
-        break
-
-# Release all space and windows once done
-vid.release()
-cv2.destroyAllWindows()
-
-#TO BE ADDED: SHESTER getting frames end
 
 import logging
 import argparse
@@ -78,14 +25,59 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 def main(args):
     root_path = Path(args.save_path)
     for video_path in root_path.rglob("*.mp4"):
-        cap = cv2.VideoCapture(str(video_path))
-        frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
-        if frame_rate == 25:
-            continue
-        logging.info(f"Start converting {video_path}, its frame rate is {frame_rate}")
+        vid = cv2.VideoCapture(str(video_path))
+        frame_rate = int(vid.get(cv2.CAP_PROP_FPS))
+        #if frame_rate == 25:
+        #    continue
+        #logging.info(f"Start converting {video_path}, its frame rate is {frame_rate}")
         video_path = video_path.resolve()
         os.system(f"ffmpeg -i '{video_path}' -r 25 /tmp/sign.mp4 -y")
-        shutil.move("/tmp/sign.mp4", str(video_path))
+        framed_vid = cv2.VideoCapture("/tmp/sign.mp4")
+        
+        # frame
+        currentframe = 1
+        
+        while (True):
+
+            # reading from frame
+            success, frame = framed_vid.read()
+
+            if success:
+                cropped_frame = frame(Rect(0, 0, frame.cols/2, frame.rows/3));
+                verse = pytesseract.image_to_string(Image.open(cropped_frame))
+                verse = PREPROCESS VERSE TO A STANDARD FORMAT #TODO SHESTER
+                if VERSE_EQUALS_TO_THE_STANDARD_FORMAT: #TODO SHESTER                                  
+                    # continue creating images until video remains                   
+                    
+                    # creates folder with verse name if it doesn't yet exists. 
+                    try:
+                        # creates folder with verse name if it doesn't yet exists. 
+                        if not os.path.exists(verse):
+                            os.makedirs(verse)
+                    # if not created then raise error
+                    except OSError:
+                        print('Error: Creating directory of data')     
+                    
+                    if not os.path.exists(verse):
+                        os.makedirs(verse)
+                    
+                    name = './f"{verse}"/images' + str("{:04d}".format(currentframe)) + '.png' #TODO SHESTER : NOT VERY SURE OF THE f"{verse}
+                    print('Creating...' + name)
+
+                    # writing the extracted images
+                    cv2.imwrite(name, frame)
+
+                    # increasing counter so that it will
+                    # show how many frames are created
+                    currentframe += 1
+                    
+            else:
+                break
+        # Release all space and windows once done
+        vid.release()
+        cv2.destroyAllWindows()        
+        
+        shutil.move("/tmp/sign.mp4", str(video_path)) #TODO SHESTER: MOVE TO A PLACE WHERE ALL FRAMED VIDEOS WILL BE. 
 
 
 if __name__ == "__main__":
